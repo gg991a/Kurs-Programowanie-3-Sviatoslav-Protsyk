035a9f28366be672fccde050369dd4942f19f68a
sviat <sviat@PS17>
2025-11-06 02:44:38 +0100

    Dodałem Lista 1

---
+++ Lista 1/Zad2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lista_1
+{
+    /// <summary>
+    /// Interaction logic for Zad2.xaml
+    /// </summary>
+    public partial class Zad2 : Window
+    {
+        public Zad2()
+        {
+            InitializeComponent();
+        }
+    }
+}
