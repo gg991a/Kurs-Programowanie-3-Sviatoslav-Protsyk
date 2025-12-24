c195da903a0849f17309cfc8d667a10fd85cd926
sviat <sviat@PS17>
2025-12-24 18:54:09 +0100

    Lista 5

---
+++ Lista 5/Lista 5/dbo/Tables/Produkty.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Produkty] (
+    [Id]          INT             IDENTITY (1, 1) NOT NULL,
+    [Nazwa]       NVARCHAR (100)  NOT NULL,
+    [Cena]        DECIMAL (10, 2) NOT NULL,
+    [KategoriaId] INT             NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([KategoriaId]) REFERENCES [dbo].[Kategorie] ([Id])
+);
+
---
+++ Lista 5/SportApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SklepSportowy
+{
+    public class Baza
+    {
+        private readonly string connectionString = @"Server=localhost\SQLEXPRESS;Database=SklepSportowy;Trusted_Connection=True;TrustServerCertificate=True;";
+        public void PolaczIPobierz()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    Console.WriteLine("--> Połączenie udane!");
+
+                    string query = @"
+                        SELECT p.Nazwa, p.Cena, k.Nazwa as Kategoria
+                        FROM Produkty p
+                        JOIN Kategorie k ON p.KategoriaId = k.Id";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Console.WriteLine("\n--- TOWARY ---");
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["Nazwa"]} - {reader["Cena"]} zł ({reader["Kategoria"]})");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("BŁĄD: " + ex.Message);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Uruchamianie programu...");
+
+            Baza baza = new Baza();
+            baza.PolaczIPobierz();
+
+            Console.ReadKey();
+        }
+    }
+}
