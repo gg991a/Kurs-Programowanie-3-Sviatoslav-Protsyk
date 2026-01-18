49abcf130e56878975723250d7614ed43e7b3a7b
sviat <sviat@PS17>
2026-01-18 16:18:52 +0100

    Zmiany 2

---
+++ Kurs-Programowanie-3-Sviatoslav-Protsyk-Lista-4/Lista 4/Lista 4/Employee.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+
+namespace Lista_4
+{
+    [Serializable]
+    public partial class Employee : ObservableObject
+    {
+        [ObservableProperty]
+        private string _firstName = string.Empty;
+
+        [ObservableProperty]
+        private string _lastName = string.Empty;
+
+        [ObservableProperty]
+        private string _position = string.Empty;
+
+        [ObservableProperty]
+        private string _imagePath = string.Empty;
+
+        public Employee() { }
+
+        public Employee(string fName, string lName, string pos, string imgPath)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Position = pos;
+            ImagePath = imgPath;
+        }
+    }
+}
