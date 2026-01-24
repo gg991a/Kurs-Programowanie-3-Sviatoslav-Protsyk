702aa0fcee38c1ff5023a19e849d12734f1cc7ff
sviat <sviat@PS17>
2026-01-24 15:21:54 +0100

    Dodałem liste

---
+++ Lista 6/SportsStoreProject/SportsStore.Api/Controllers/ProductsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Api.Models;
+
+namespace SportsStore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> _products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Piłka nożna", Category = "Sporty drużynowe", Price = 800 },
+            new Product { Id = 2, Name = "Rakieta tenisowa", Category = "Tenis", Price = 1500 }
+        };
+
+        [HttpGet] 
+        public ActionResult<IEnumerable<Product>> Get() => Ok(_products);
+
+        [HttpPost] 
+        public IActionResult Post([FromBody] Product product)
+        {
+            product.Id = _products.Count > 0 ? _products.Max(p => p.Id) + 1 : 1;
+            _products.Add(product);
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")] 
+        public IActionResult Put(int id, [FromBody] Product updatedProduct)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product == null) return NotFound();
+            product.Name = updatedProduct.Name;
+            product.Category = updatedProduct.Category;
+            product.Price = updatedProduct.Price;
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")] 
+        public IActionResult Delete(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product == null) return NotFound();
+            _products.Remove(product);
+            return Ok();
+        }
+    }
+}
---
+++ Lista 6/SportsStoreProject/SportsStore.Api/Program.cs
@@ -0,0 +1,32 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseAuthorization();
+app.MapControllers();
+
+Console.WriteLine("\n*************************************************");
+Console.WriteLine("INTERFEJS KLIENTA DOSTUPNY POD ADRESEM:");
+Console.WriteLine("https://localhost:7273/index.html");
+Console.WriteLine("*************************************************\n");
+
+app.Run();
