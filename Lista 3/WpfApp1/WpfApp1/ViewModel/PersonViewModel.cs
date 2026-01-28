6074f02884b40444a8a3c89f8c7b4d2f557e9a75
Svitoslav Protsyk <g9980302@gmail.com>
2026-01-28 18:39:51 +0100

    Dodałem lista 3 (#3)
    * Dodałem lista 3
    
    * Zmiany lista 3
    
    ---------
    
    Co-authored-by: sviat <sviat@PS17>

---
+++ Lista 3/WpfApp1/WpfApp1/ViewModel/PersonViewModel.cs
@@ -0,0 +1,41 @@
+﻿using WpfApp1.Model;
+using System.ComponentModel;
+
+namespace WpfApp1.ViewModel
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person _person;
+        public Person Person => _person;
+
+        public PersonViewModel(Person person)
+        {
+            _person = person;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string FirstName
+        {
+            get => _person.FirstName;
+            set { _person.FirstName = value; OnPropertyChanged(nameof(FirstName)); }
+        }
+
+        public string LastName
+        {
+            get => _person.LastName;
+            set { _person.LastName = value; OnPropertyChanged(nameof(LastName)); }
+        }
+
+        public string PESEL
+        {
+            get => _person.PESEL;
+            set { _person.PESEL = value; OnPropertyChanged(nameof(PESEL)); }
+        }
+    }
+}
