438d9bf4d03b2b31f427a549be3bc74724928dca
Svitoslav Protsyk <g9980302@gmail.com>
2026-01-28 18:41:17 +0100

    Dodałem liste (#6)
    Co-authored-by: sviat <sviat@PS17>

---
+++ Lista 6/SportsStoreProject/SportsStore.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using RestSharp;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+string baseUrl = config["ApiSettings:BaseUrl"] ?? "https://localhost:7157/";
+var client = new RestClient(baseUrl);
+
+Console.WriteLine("--- SPORTS STORE CLIENT ---");
+var request = new RestRequest("api/Products", Method.Get);
+var response = await client.ExecuteAsync(request);
+
+if (response.IsSuccessful && response.Content != null)
+{
+    var options = new JsonSerializerOptions { WriteIndented = true };
+    var json = JsonSerializer.Deserialize<JsonElement>(response.Content);
+    Console.WriteLine("Lista produktów:\n" + JsonSerializer.Serialize(json, options));
+}
+
+Console.WriteLine("\nNaciśnij klawisz...");
+Console.ReadKey();
